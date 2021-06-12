626fea3308454df408b012753fa1219857606403
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-12 03:22:25 +0300

    More code

---
+++ src/main/java/com/annakhuseinova/springcloudstreams/services/KafkaListenerService.java
@@ -0,0 +1,21 @@
+package com.annakhuseinova.springcloudstreams.services;
+
+import com.annakhuseinova.springcloudstreams.bindings.KafkaListenerBinding;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@EnableBinding(KafkaListenerBinding.class)
+public class KafkaListenerService {
+
+    @StreamListener("input-channel-1")
+    public void process(KStream<String, String> input){
+        input.foreach((key, value) -> {
+            log.info(String.format("Key: %s, Value: %s", key, value));
+        });
+    }
+}
